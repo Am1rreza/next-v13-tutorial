d7652ba4dce651bf925d0eda481ff1850e5f14fb
Amirreza <amirreza.m13825@gmail.com>
2023-05-28 19:59:31 +0330

    implement routing system

---
+++ src/app/about/page.jsx
@@ -0,0 +1,17 @@
+const AboutPage = () => {
+  return (
+    <div>
+      <h1 className="font-bold text-3xl">About Traversy Media</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vero maiores,
+        facilis, animi a laudantium tempore laboriosam libero, similique unde
+        saepe at. Libero at tempore id tenetur, minus atque ea rem blanditiis
+        magnam nesciunt consequatur voluptas unde veritatis dignissimos
+        similique laboriosam. Quisquam laudantium soluta repudiandae aspernatur!
+        Iste vitae possimus numquam sed.
+      </p>
+    </div>
+  );
+};
+
+export default AboutPage;
---
+++ src/app/about/team/page.jsx
@@ -0,0 +1,15 @@
+const TeamPage = () => {
+  return (
+    <div>
+      <h1 className="font-bold text-3xl">Our Team</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Expedita culpa
+        sunt quasi molestiae placeat, commodi vel deleniti enim, repellat
+        asperiores earum quidem. Ex qui voluptatibus nihil! Id corporis nihil
+        similique.
+      </p>
+    </div>
+  );
+};
+
+export default TeamPage;
