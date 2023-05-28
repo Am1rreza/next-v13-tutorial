155f6019d6e89d7ea128861d8af3e5e0d06cd249
Amirreza <amirreza.m13825@gmail.com>
2023-05-28 21:31:11 +0330

    loading component added

---
+++ src/app/loading.jsx
@@ -0,0 +1,9 @@
+const LoadingPage = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="border-4 border-[rgba(0,0,0,0.1)] border-l-blue-500 rounded-full h-10 animate-spin"></div>
+    </div>
+  );
+};
+
+export default LoadingPage;
